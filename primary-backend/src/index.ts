a74f511d0af8f8981ee9d043b005130eb36e75c0
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-08-16 05:28:20 +0530

    primary backend: trigger and action router

--- primary-backend/src/index.ts
+++ primary-backend/src/index.ts
@@ -2,6 +2,8 @@ import { configDotenv } from "dotenv";
 import express from "express";
 import cors from "cors";
 import { userRouter } from "./router/user";
+import { triggerRouter } from "./router/trigger";
+import { actionRouter } from "./router/action";
 
 configDotenv();
 
@@ -12,6 +14,10 @@ app.use(cors());
 const PORT = process.env.PORT;
 
 app.use("/api/v1/user", userRouter);
+
+app.use("/api/v1/trigger", triggerRouter);
+
+app.use("/api/v1/action", actionRouter);
 
 app.listen(PORT, () => {
   console.log(`Backend running on ${PORT}.`)

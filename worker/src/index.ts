26fac14a113751e076fac376ddaa2811b7a48bad
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-08-20 23:12:58 +0530

    parser, consumer & producer

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,5 +1,11 @@
+import { configDotenv } from "dotenv";
+import { PrismaClient } from "@prisma/client";
+import { JsonObject } from "@prisma/client/runtime/library";
 import { Kafka } from "kafkajs";
+import { parse } from "./parser";
 
+configDotenv();
+const prismaClient = new PrismaClient();
 const TOPIC_NAME = "zap-events";
 
 const kafka = new Kafka({
@@ -10,7 +16,8 @@ const kafka = new Kafka({
 async function main() {
   const consumer = kafka.consumer({ groupId: "main-worker" });
   await consumer.connect();
-
+  const producer =  kafka.producer();
+  await producer.connect();
   await consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true });
 
   await consumer.run({
@@ -22,7 +29,38 @@ async function main() {
         value: message.value?.toString(),
       });
 
-      await new Promise((r) => setTimeout(r, 500));
+      if (!message.value?.toString()) {
+        return;
+      }
+
+      const parsedValue = JSON.parse(message.value?.toString());
+          const zapRunId = parsedValue.zapRunId;
+          const stage = parsedValue.stage;
+
+          const zapRunDetails = await prismaClient.zapRun.findFirst({
+            where: {
+              id: zapRunId
+            },
+            include: {
+              zap: {
+                include: {
+                  actions: {
+                    include: {
+                      type: true
+                    }
+                  }
+                }
+              },
+            }
+          });
+          const currentAction = zapRunDetails?.zap.actions.find(x => x.sortingOrder === stage);
+
+          if (!currentAction) {
+            console.log("Current action not found?");
+            return;
+          }
+
+          const zapRunMetadata = zapRunDetails?.metadata;
 
       console.log("processing done");
 

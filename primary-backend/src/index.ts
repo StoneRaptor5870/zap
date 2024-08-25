7b2f357bd7dc99d614ca500ddce00457ef5c795b
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-08-26 00:43:09 +0530

    dist primary backend

--- primary-backend/src/index.ts
+++ primary-backend/src/index.ts
@@ -1,5 +1,5 @@
 import { configDotenv } from "dotenv";
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import { userRouter } from "./router/user";
 import { zapRouter } from "./router/zap";
@@ -21,6 +21,10 @@ app.use("/api/v1/zap", zapRouter);
 app.use("/api/v1/trigger", triggerRouter);
 
 app.use("/api/v1/action", actionRouter);
+
+app.use("/", (req: Request, res: Response) => {
+  res.send("<h1>Hello from the zap primary backend server!</h1>");
+});
 
 app.listen(PORT, () => {
   console.log(`Backend running on ${PORT}.`)
